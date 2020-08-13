1dc88e01a47445a32f0f4bed37212db620721873
Miguel <miguelsousalima@hotmail.com>
2020-08-13 12:31:20 -0300

    Section e Header concluido

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+  Container,
+  Content,
+  HeaderWrapper,
+  Header,
+  DropboxLogo,
+} from "./styles";
+
+interface Props {
+  variant: "blue" | "beige" | "white" | "black";
+  title: string;
+  description: string;
+}
+
+const Section: React.FC<Props> = ({ title, variant, description }) => {
+  const buttonVariant = Math.round(Math.random());
+
+  return (
+    <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+
+          <button>{buttonVariant === 0 ? "Acessar" : "Interagir"}</button>
+        </Header>
+      </HeaderWrapper>
+
+      <Content>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Content>
+    </Container>
+  );
+};
+
+export default Section;
